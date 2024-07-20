fbd8b48c98d7a6f3f5e1ed60f69b463786bff263
Dimitar Ivanov <riseabove92@yahoo.com>
2024-07-20 20:11:10 +0300

    Stages list style update

--- client/src/components/stages/stage-list/StageList.jsx
+++ client/src/components/stages/stage-list/StageList.jsx
@@ -5,11 +5,11 @@ export default function StageList() {
                 <section className="w-full flex xl:flex-row flex-col justify-center min-h-[80vh] gap-10 bg-black relative bg-gradient-to-b from-purple-800 to-rgb-400">
                     <div className="container mx-auto px-4 py-8 text-white pt-32">
                         <div className="text-3xl font-bold mb-6">
-                            <h2 className="text-3xl font-bold mb-6 text-center">
+                            <h2 className="text-4xl font-bold mb-6 text-center">
                                 Stages List
                             </h2>
                         </div>
-                        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 w-full max-w-7xl mx-auto">
+                        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 w-full max-w-7xl mx-auto ">
                             {/* News Card Start */}
                             <div className="border-4 border-gray-700 rounded-lg overflow-hidden shadow-lg">
                                 <img
