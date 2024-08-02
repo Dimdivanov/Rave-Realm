056d7aac83ab2c64c1eaef49d2180f7ab0428167
Dimitar Ivanov <riseabove92@yahoo.com>
2024-08-02 23:08:07 +0300

    TicketDetails style edit

--- client/src/components/getTickets/ticket-details/TicketDetails.jsx
+++ client/src/components/getTickets/ticket-details/TicketDetails.jsx
@@ -10,7 +10,7 @@ export default function TicketDetails() {
     return (
         <>
             <div className="min-h-screen bg-gradient-to-b from-purple-700 to-black flex items-center justify-center py-10">
-                <div className="w-full max-w-4xl p-6 bg-gray-900 bg-opacity-80 rounded-lg shadow-md backdrop-blur-md flex flex-col lg:flex-row">
+                <div className="w-full max-w-4xl p-24 bg-gray-900 bg-opacity-80 rounded-lg shadow-md backdrop-blur-md flex flex-col lg:flex-row">
                     <h2 className="text-3xl font-semibold mb-6 text-center text-white w-full lg:hidden">
                         {ticketDetails.ticketName}
                     </h2>
@@ -18,7 +18,7 @@ export default function TicketDetails() {
                         <img
                             src={ticketDetails.ticketImgUrl}
                             alt="Ticket Image"
-                            className="rounded-lg w-full h-auto object-cover lg:max-h-96"
+                            className="rounded-lg w-full h-full object-cover lg:max-h-96"
                         />
                     </div>
                     <div className="w-full lg:w-1/2 lg:pl-10 mt-6 lg:mt-0">
