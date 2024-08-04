d91ff56c2c5a281eb5805127ac7f2ecc1921c350
Dimitar Ivanov <riseabove92@yahoo.com>
2024-08-04 21:50:24 +0300

    sitemapmenu nick added

--- client/src/components/settingsMenu/SettingsMenu.jsx
+++ client/src/components/settingsMenu/SettingsMenu.jsx
@@ -1,22 +1,21 @@
 import SiteMapMenu from './site-map-menu/SiteMapMenu';
 import SiteMapContent from './site-map-content/SiteMapContent';
+import { useContext } from 'react';
+import { AuthContext } from '../../contexts/AuthContext';
 
 export default function SettingsMenu() {
+    const userData = useContext(AuthContext);
+
     return (
         <>
-            <>
-                {/* Create Nested routes */}
-                {/* re-use navigation panel*/}
+            <div className="bg-gradient-to-b from-purple-800 to-black text-white min-h-screen flex justify-center items-center">
+                {/* Main Container */}
+                <div className="w-full max-w-screen-lg p-4 md:flex md:space-x-4">
+                    <SiteMapMenu userData={userData}/>
 
-                <div className="bg-gradient-to-b from-purple-800 to-black text-white min-h-screen flex justify-center items-center">
-                    {/* Main Container */}
-                    <div className="w-full max-w-screen-lg p-4 md:flex md:space-x-4">
-                        <SiteMapMenu />
-
-                        <SiteMapContent />
-                    </div>
+                    <SiteMapContent />
                 </div>
-            </>
+            </div>
         </>
     );
 }
--- client/src/components/settingsMenu/site-map-menu/SiteMapMenu.jsx
+++ client/src/components/settingsMenu/site-map-menu/SiteMapMenu.jsx
@@ -1,11 +1,11 @@
 import { Link } from 'react-router-dom';
 
-export default function SiteMapMenu() {
+export default function SiteMapMenu({ userData }) {
     return (
         <>
             {/* Sidebar */}
             <div className="w-full md:w-1/4 p-4 space-y-4 bg-black bg-opacity-25 rounded-lg">
-                <div className="text-xl"></div>
+                <div className="text-xl">{userData.email}</div>
                 <div className="flex items-center"></div>
                 <nav className="mt-8">
                     <ul className="space-y-4">
